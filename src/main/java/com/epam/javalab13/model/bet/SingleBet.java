29a7b4aeab118c88b6c481f42a9f0b3c0959d77b
roman <romanmr01@gmail.com>
2016-09-02 18:00:38 +0300

    change encoding

--- src/main/java/com/epam/javalab13/model/bet/SingleBet.java
+++ src/main/java/com/epam/javalab13/model/bet/SingleBet.java
@@ -16,7 +16,7 @@ public class SingleBet {
 	private BetTotalGoals betTotalGoals;
 	private BetPlayer betPlayer;
 
-	// не може бути одна ставка на багатьох гравців
+	
 	// private Player player;
 	public SingleBet() {
 		super();
