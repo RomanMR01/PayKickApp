d0d2ae9d18cb07a6fdd16bd88619976b8bc1c106
Petro <kotpein@gmail.com>
2016-09-02 21:33:49 +0300

    Added DAO<E> interface

---
+++ src/main/java/com/epam/javalab13/dao/UserDAO.java
@@ -0,0 +1,47 @@
+package com.epam.javalab13.dao;
+
+import java.util.List;
+
+import com.epam.javalab13.model.User;
+
+public class UserDAO implements DAO<User>{
+
+	@Override
+	public User findById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean create(User e) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean update(User e) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean delete(User e) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean delete(int id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+
+	@Override
+	public List<User> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+}
--- src/main/java/com/epam/javalab13/model/bet/BetPlayer.java
+++ src/main/java/com/epam/javalab13/model/bet/BetPlayer.java
@@ -6,39 +6,49 @@ public class BetPlayer {
 	private int id;
 	private SingleBet singleBet;
 	private Player player;
+
 	public BetPlayer() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
+
 	public BetPlayer(SingleBet singleBet, Player player) {
 		super();
 		this.singleBet = singleBet;
 		this.player = player;
 	}
+
 	public BetPlayer(int id, SingleBet singleBet, Player player) {
 		super();
 		this.id = id;
 		this.singleBet = singleBet;
 		this.player = player;
 	}
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public SingleBet getSingleBet() {
 		return singleBet;
 	}
+
 	public void setSingleBet(SingleBet singleBet) {
 		this.singleBet = singleBet;
 	}
+
 	public Player getPlayer() {
 		return player;
 	}
+
 	public void setPlayer(Player player) {
 		this.player = player;
 	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -48,6 +58,7 @@ public class BetPlayer {
 		result = prime * result + ((singleBet == null) ? 0 : singleBet.hashCode());
 		return result;
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -71,6 +82,7 @@ public class BetPlayer {
 			return false;
 		return true;
 	}
+
 	@Override
 	public String toString() {
 		return "BetPlayer [id=" + id + ", singleBet=" + singleBet + ", player=" + player + "]";
