8c58f801780fcd430e6ec0506a5fbb0051311d87
roman <romanmr01@gmail.com>
2016-09-05 05:10:46 +0300

    fix Test

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,35 +1,26 @@
-import java.sql.SQLException;
+import com.epam.javalab13.dao.ConnectionPool;
+import com.epam.javalab13.util.DateConverter;
 
-import com.epam.javalab13.dao.game.PlayerCoefficientDAO;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Timestamp;
+import java.util.Calendar;
+import java.util.Date;
 
 /*
  * Test for checking sql query
  */
 public class Test {
     public static void main(String[] args) throws SQLException {
-//        Statement statement = ConnectionPool.getConnection().createStatement();
-//        ResultSet set = statement.executeQuery("SELECT tb.user_id,tb.award FROM totalizator.total_bet tb JOIN totalizator.single_bet sb ON sb.total_bet_id=tb.id WHERE sb.game_id=1");
-//
-//        while (set.next()){
-//            System.out.println(set.getInt("user_id") + "\t" + set.getString("award"));//1 1000 for 3:00 PM
-//
-//        }
-    	
-//    	GameDAO dao = new GameDAO();
-////    	System.out.println(dao.findAll());
-//    	
-//    	Team team1=new Team(1,"","","",0,0,0,null);
-//    	Team team2=new Team(2,"","","",0,0,0,null);
-//		Game game = new Game("гра1", "Київ", new Date(), team1, team2, Status.NEW);
-////		dao.create(game);
-//		game=dao.findById(1);
-//		game.setDate(new Date());
-//		dao.update(game);
-//		System.out.println(game);
-		
-    	PlayerCoefficientDAO dao = new PlayerCoefficientDAO();
-		
-    	System.out.println(dao.findAll());
-    	
+        Statement statement = ConnectionPool.getConnection().createStatement();
+        ResultSet set = statement.executeQuery("SELECT tb.user_id,tb.award FROM totalizator.total_bet tb JOIN totalizator.single_bet sb ON sb.total_bet_id=tb.id WHERE sb.game_id=1");
+
+        while (set.next()){
+            System.out.println(set.getInt("user_id") + "\t" + set.getString("award"));//1 1000 for 3:00 PM
+
+        }
+
     }
 }
