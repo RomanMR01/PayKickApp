fdca1e1b1ca01865100072b58aace67e7431689c
roman <romanmr01@gmail.com>
2016-09-05 15:12:48 +0300

    added ResultCoefficientTransformer

--- src/main/java/com/epam/javalab13/transformer/game/ResultCoefficientTransformer.java
+++ src/main/java/com/epam/javalab13/transformer/game/ResultCoefficientTransformer.java
@@ -1,57 +1,50 @@
 package com.epam.javalab13.transformer.game;
 
+import com.epam.javalab13.dao.UserDAO;
+import com.epam.javalab13.dao.game.GameDAO;
+import com.epam.javalab13.dao.game.TeamDAO;
+import com.epam.javalab13.model.User;
+import com.epam.javalab13.model.bet.Result;
+import com.epam.javalab13.model.game.Game;
+import com.epam.javalab13.model.game.ResultCoefficient;
+import com.epam.javalab13.model.game.Status;
+import com.epam.javalab13.model.game.Team;
+import com.epam.javalab13.transformer.Transformer;
+
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.log4j.Logger;
-
-import com.epam.javalab13.model.bet.Result;
-import com.epam.javalab13.model.game.ResultCoefficient;
-import com.epam.javalab13.transformer.Transformer;
-
-public class ResultCoefficientTransformer implements Transformer<ResultCoefficient> {
-
-    private static final Logger logger = Logger.getLogger(ResultCoefficientTransformer.class);
-    private GameTransformer gameTransformer;
-
+/**
+ * Created by Vikno on 9/5/2016.
+ */
+public class ResultCoefficientTransformer implements Transformer<ResultCoefficient>{
     @Override
-    public ResultCoefficient getOne(ResultSet rs) {
-        ResultCoefficient rc = null;
-        try {
-            if (rs.next()) {
-                rc = createResultCoefficient(rs);
-            }
-        } catch (SQLException e) {
-            logger.error("failed to instantiate ResultCoefficient from ResultSet",e);
-        }
-        return rc;
-    }
-
-    private ResultCoefficient createResultCoefficient(ResultSet rs) throws SQLException {
-        ResultCoefficient rc = new ResultCoefficient();
-        rc.setId(rs.getInt("result_id"));
-        rc.setCoefficient(rs.getDouble("coefficient"));
-        rc.setResult(Result.valueOf(rs.getString("result")));
-        if (gameTransformer == null) {
-            gameTransformer = new GameTransformer();
-        }
-        rc.setGame(gameTransformer.createGame(rs));
-        return rc;
+    public ResultCoefficient getOne(ResultSet rs) throws SQLException {
+        return null;
     }
 
     @Override
-    public List<ResultCoefficient> getAll(ResultSet rs) {
-        List<ResultCoefficient> rcList = new ArrayList<>();
-        try {
-            while(rs.next()){
-                rcList.add(createResultCoefficient(rs));
-            }
-        } catch (SQLException e) {
-            logger.error("failed to instantiate ResultCoefficient from ResultSet",e);
-        }
-        return rcList;
-    }
+    public List<ResultCoefficient> getAll(ResultSet rs) throws SQLException {
+        List<ResultCoefficient> resultCoefficients = new ArrayList<>();
 
+
+        while (rs.next()) {
+            ResultCoefficient resultCoefficient = new ResultCoefficient();
+            resultCoefficient.setId(rs.getInt("id"));
+
+            Game game = new Game();
+            GameDAO gameDAO = new GameDAO();
+            game = gameDAO.getGamesById(rs.getInt("game_id"));
+
+            resultCoefficient.setGame(game);
+            resultCoefficient.setResult(Result.valueOf(rs.getString("result")));
+            resultCoefficient.setCoefficient(rs.getDouble("coefficient"));
+
+            resultCoefficients.add(resultCoefficient);
+        }
+
+        return resultCoefficients;
+    }
 }
