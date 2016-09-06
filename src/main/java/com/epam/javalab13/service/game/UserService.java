56178fd878b696c77a31189bf1d8f8fb71a48793
roman <romanmr01@gmail.com>
2016-09-06 14:08:02 +0300

    added new methods and remove findUserByLoginAndPassword

--- src/main/java/com/epam/javalab13/service/game/UserService.java
+++ src/main/java/com/epam/javalab13/service/game/UserService.java
@@ -1,6 +1,8 @@
 package com.epam.javalab13.service.game;
 
 import com.epam.javalab13.dao.UserDAO;
+import com.epam.javalab13.model.Language;
+import com.epam.javalab13.model.Role;
 import com.epam.javalab13.model.User;
 
 import java.sql.SQLException;
@@ -11,12 +13,12 @@ import java.sql.SQLException;
 public class UserService {
     UserDAO userDAO=new UserDAO();
 
-    public void addUser(User user) throws SQLException {
-        userDAO.addUser(user);
-    }
-
-    public User findUserByLoginAndPassword(String login, String password) throws SQLException {
-        return userDAO.findUserByLoginAndPassword(login, password);
+    public void addUser(User user){
+        try {
+            userDAO.addUser(user);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public User getUserByLogin(String login){
@@ -32,5 +34,69 @@ public class UserService {
         }
 
         return user;
+    }
+
+    public String getUserLanguage(String login){
+        User u = new User();
+        u.setLogin(login);
+
+        String lang = "en_EN";
+
+        User user = getUserByLogin(login);
+            if(user!=null && user.getLanguage()!=null){
+                lang = user.getLanguage().toString();
+            }
+
+        return lang;
+    }
+
+    public void changeUserLanguage(String login,String lang){
+
+        User u = getUserByLogin(login);
+        u.setLanguage(Language.valueOf(lang));
+        try {
+            userDAO.updateUser(u, UserDAO.UpdateUserType.LANGUAGE);
+        } catch (SQLException e) {
+           e.printStackTrace();
+        }
+    }
+
+    public void setUserBan(int userId){
+        User user = new User();
+        user.setId(userId);
+        user.setBanned(true);
+
+        try {
+            userDAO.updateUser(user, UserDAO.UpdateUserType.BAN);
+
+        } catch (SQLException e) {
+         e.getStackTrace();
+        }
+    }
+
+
+    public void resetUserBan(int userId){
+        User user = new User();
+        user.setId(userId);
+        user.setBanned(false);
+
+        try {
+            userDAO.updateUser(user, UserDAO.UpdateUserType.BAN);
+
+        } catch (SQLException e) {
+            e.getStackTrace();
+        }
+    }
+
+
+    public void changeUserRole(int userId,String role){
+        User user = new User();
+        user.setId(userId);
+        user.setRole(Role.valueOf(role));
+        try {
+            userDAO.updateUser(user, UserDAO.UpdateUserType.ROLE);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
