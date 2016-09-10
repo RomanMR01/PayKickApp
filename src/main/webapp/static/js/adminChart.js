69c89a496e1b4429f26437f4f8fb0d64411979a2
roman <romanmr01@gmail.com>
2016-09-10 17:54:55 +0300

    change chart for main statistics

--- src/main/webapp/static/js/adminChart.js
+++ src/main/webapp/static/js/adminChart.js
@@ -1,87 +1,91 @@
 $(document).ready(function () {
     console.log("admin js");
-    var diagram_info = [];
 
-    var win = 0;
-    var loss = 0;
+    var dateLabels = [];
+    var  backgroundColors = [];
+    var  borderColors = [];
+    var  profitData = [];
+    var  hoverBackgroundColors = [];
     $.ajax({
         type: "POST",
-        data: {"type": "all_bats"},
+        data: {"type": "MAIN_CHART"},
         url: "chart",
-        success: function (data) {
-            $.each(data, function (index, value) {
-                diagram_info.push([Date.parse(value.date), value.award]);
-                if (value.status === "WON") {
-                    win = win + value.award;
-                    console.log("win" + win);
-                } else if (value.status === "LOST") {
-                    loss = loss - value.award;
-                    console.log("loss" + loss);
+        success: function (resp) {
+            $.each(resp, function (index, value) {
+                var time = new Date(Date.parse(value.date));
+                dateLabels.push(time.getDate() + "." + (time.getMonth()+1) + "." + time.getFullYear());
+
+                var profit = value.profit;
+
+                if(profit==0){
+                    backgroundColors.push('rgba(0, 0, 255, 0.3)');
+                    borderColors.push('rgba(0,0,200,0.5)');
+                    hoverBackgroundColors.push('rgba(0,0,200,0.5)');
+                }else if(profit>0){
+                    backgroundColors.push('rgba(0, 255, 0, 0.3)');
+                    borderColors.push('rgba(0,200,0,0.5)');
+                    hoverBackgroundColors.push('rgba(0,200,0,0.5)');
+                }else if(profit<0){
+                    backgroundColors.push('rgba(255, 0, 0, 0.3)');
+                    borderColors.push('rgba(200, 0, 0, 0.5)');
+                    hoverBackgroundColors.push('rgba(200, 0, 0, 0.5)');
+                }
+
+                profitData.push(profit);
+                // console.log(value.profit)
+                // if (value.status === "WON") {
+                //     win = win + value.award;
+                //     console.log("win" + win);
+                // } else if (value.status === "LOST") {
+                //     loss = loss - value.award;
+                //     console.log("loss" + loss);
+                // }
+            });
+
+            var ctx = document.getElementById("mainChart");
+            
+            var data = {
+                labels: dateLabels,
+                datasets: [
+                    {
+                        label: "Profit: ",
+                        backgroundColor: backgroundColors,
+                        borderColor: borderColors,
+                        hoverBackgroundColor:hoverBackgroundColors,
+                        borderWidth: 3,
+                        data: profitData,
+                    },
+                ]
+            };
+            var myBarChart = new Chart(ctx, {
+                type: 'bar',
+                data: data,
+                options: {
+                    legend: {
+                        display: false,
+                    },
+                    scales: {
+                        xAxes: [{
+                            stacked: true,
+                            scaleLabel: {
+                                display: true,
+                                labelString: 'DATE'
+                            }
+                        }],
+                        yAxes: [{
+                            stacked: true,
+                            scaleLabel: {
+                                display: true,
+                                labelString: 'PROFIT'
+                            }
+                        }],
+
+                    }
+
                 }
             });
-            chart_info = [
-                ['Wins', win],
-                ['Loss', loss],
-            ];
 
-            /*diagram*/
-            $('#admin_diagram').highcharts({
-                chart: {
-                    zoomType: 'x'
-                },
-                title: {
-                    text: 'Profits'
-                },
-                subtitle: {
-                    text: document.ontouchstart === undefined ?
-                        'Click and drag in the plot area to zoom in' : 'Pinch the chart to zoom in'
-                },
-                xAxis: {
-                    type: 'datetime'
-                },
-                yAxis: {
-                    title: {
-                        text: '$'
-                    }
-                },
-                legend: {
-                    enabled: false
-                },
-                plotOptions: {
-                    area: {
-                        fillColor: {
-                            linearGradient: {
-                                x1: 0,
-                                y1: 0,
-                                x2: 0,
-                                y2: 1
-                            },
-                            stops: [
-                                [0, Highcharts.getOptions().colors[0]],
-                                [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
-                            ]
-                        },
-                        marker: {
-                            radius: 2
-                        },
-                        lineWidth: 1,
-                        states: {
-                            hover: {
-                                lineWidth: 1
-                            }
-                        },
-                        threshold: null
-                    }
-                },
-                credits: {
-                    enabled: false
-                },
-                series: [{
-                    type: 'area',
-                    name: 'win amount',
-                    data: diagram_info
-                }]
-            });
+
         }
     });
 });
