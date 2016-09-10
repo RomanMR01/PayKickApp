ef2afa528ab468a35966535f4fc90459517ddd57
roman <romanmr01@gmail.com>
2016-09-10 22:01:20 +0300

    return previous code

--- src/main/java/com/epam/javalab13/servlet/ChartServlet.java
+++ src/main/java/com/epam/javalab13/servlet/ChartServlet.java
@@ -1,10 +1,8 @@
 package com.epam.javalab13.servlet;
 
 import com.epam.javalab13.dao.bet.TotalBetDAO;
-import com.epam.javalab13.dao.game.GameDAO;
 import com.epam.javalab13.model.User;
 import com.epam.javalab13.model.bet.TotalBet;
-import com.epam.javalab13.model.game.Game;
 import com.epam.javalab13.model.game.Team;
 import com.epam.javalab13.service.game.TeamService;
 import com.google.gson.Gson;
@@ -18,7 +16,6 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
 /**
@@ -32,6 +29,8 @@ public class ChartServlet extends HttpServlet {
         requestDispatcher = req.getRequestDispatcher("chart.jsp");
         requestDispatcher.forward(req, resp);
     }
+
+
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
@@ -44,21 +43,21 @@ public class ChartServlet extends HttpServlet {
 
         if (chartType!=null){
             switch (chartType){
-            case "user_bat":
-                List<TotalBet> allBets;
-                try {
+                case "user_bat":
+                    List<TotalBet> allBets;
+                    try {
 
-                    User user=new User();
-                    if (req.getParameter("id")!=null){
-                        user.setId(Integer.valueOf(req.getParameter("id")));
+                        User user=new User();
+                        if (req.getParameter("id")!=null){
+                            user.setId(Integer.valueOf(req.getParameter("id")));
+                        }
+
+                        allBets = totalBetDAO.getTotalBetsForUser(TotalBetDAO.GetTotalBetsType.ALL, user);
+                        json = gson.toJson(allBets);
+                    } catch (SQLException e) {
+                        logger.warn("Exception while getting all TotalBets:",e);
                     }
-
-                    allBets = totalBetDAO.getTotalBetsForUser(TotalBetDAO.GetTotalBetsType.ALL, user);
-                    json = gson.toJson(allBets);
-                } catch (SQLException e) {
-                    logger.warn("Exception while getting all TotalBets:",e);
-                }
-                break;
+                    break;
                 case "team_win":
                     Team team=new Team();
                     team.setName("Динамо");
@@ -70,27 +69,17 @@ public class ChartServlet extends HttpServlet {
                     }
                     json = gson.toJson(resultTeam);
                     break;
-                case "MAIN_CHART":
-                    List<Game> finishedGames;
-                    GameDAO gameDAO = new GameDAO();
+                case "all_bats":
+                    List<TotalBet> all;
                     try {
-                        finishedGames = gameDAO.getGamesByStatus(GameDAO.GetGamesType.FINISHED);
-                        Collections.sort(finishedGames, new Comparator<Game>() {
-                            @Override
-                            public int compare(Game o1, Game o2) {
-                                return o1.getDate().compareTo(o2.getDate());
-                            }
-                        });
-                        json = gson.toJson(finishedGames);
-
-                        System.out.println(json);
+                        all = totalBetDAO.getAllTotalBets(TotalBetDAO.GetTotalBetsType.ALL);
+                        Collections.sort(all);
+                        json = gson.toJson(all);
                     } catch (SQLException e) {
-                        logger.warn("Exception while getting finished games:",e);
+                        logger.warn("Exception while getting all TotalBets:",e);
                     }
             }
         }
-
-        System.out.println(json);
         resp.setContentType("application/json");// set content to json
         resp.getWriter().write(json);
     }
