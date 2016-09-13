38642f15a4140054b55185bf53c0370d621c17ad
roman <romanmr01@gmail.com>
2016-09-13 23:55:51 +0300

    added validation if player are not a member of team

--- src/main/java/com/epam/javalab13/servlet/admin/AddWinnerServlet.java
+++ src/main/java/com/epam/javalab13/servlet/admin/AddWinnerServlet.java
@@ -4,6 +4,7 @@ import com.epam.javalab13.dao.game.PlayerDAO;
 import com.epam.javalab13.model.game.Game;
 import com.epam.javalab13.model.game.Goal;
 import com.epam.javalab13.model.game.Player;
+import com.epam.javalab13.model.game.Team;
 import com.epam.javalab13.service.game.GameService;
 import com.epam.javalab13.service.game.GoalService;
 import com.epam.javalab13.service.game.PlayerService;
@@ -40,9 +41,9 @@ public class AddWinnerServlet extends HttpServlet{
 
         logger.info("Admin " + session.getAttribute("login") + " add game results for game:" + gameId);
 
-        if((gameId==null) || (firstTeamId==null) || (secondTeamId==null) || (firstTeamScore==null) || (secondTeamScore==null)){
+        resp.setCharacterEncoding("UTF-8");
 
-            resp.setCharacterEncoding("UTF-8");
+        if((gameId==null) || (firstTeamId==null) || (secondTeamId==null) || (firstTeamScore==null) || (secondTeamScore==null)){
             resp.getWriter().write("{ \"status\": \"FAIL\",\"message\":\"Some data is empty!\"}");
             return;
         }else{
@@ -54,36 +55,67 @@ public class AddWinnerServlet extends HttpServlet{
             int scoreTwo = Integer.parseInt(secondTeamScore);
 
 
-            List<Player> firstTeam = new ArrayList<>();
-            List<Player> secondTeam = new ArrayList<>();
-
             PlayerService playerService = new PlayerService();
 
+            Team one = new Team();
+            one.setId(firstTeamID);
+            List<Player> allFirstTeamPlayers = playerService.getPlayersByTeam(one);//All players at first team
+
+            Team two = new Team();
+            two.setId(secondTeamID);
+            List<Player> allSecondTeamPlayers = playerService.getPlayersByTeam(two);//All players at second team
+
+            List<Player> firstTeamGoals = new ArrayList<>();//Players that score goals for first team
+            List<Player> secondTeamGoals = new ArrayList<>();//Players that score goals for second team
 
             if(0!=scoreOne){
-                String [] oneTeam = firstTeamPlayers.split(";");
+                String [] oneTeam = firstTeamPlayers.split(";");//All players that score goals at first team
 
+                //Search if such player exist in system(db) and then (if exists) search this player in first team
                 for(String playerName:oneTeam){
                     Player player = playerService.getPlayerByName(playerName);
                     if(player!=null) {
-                        firstTeam.add(player);
+                        boolean isThisPlayerAtTeam = false;
+                        for(Player p:allFirstTeamPlayers){
+                            if(p.getId()==player.getId()){
+                                isThisPlayerAtTeam = true;
+                                break;
+                            }
+                        }
+                        if(isThisPlayerAtTeam) {
+                            firstTeamGoals.add(player);
+                        }else{
+                            resp.getWriter().write("{ \"status\": \"FAIL\",\"message\":\"" + playerName + " is not a member of first team!\"}");
+                            return;
+                        }
                     }else{
-                        resp.setCharacterEncoding("UTF-8");
-                        resp.getWriter().write("{ \"status\": \"FAIL\",\"message\":\"Incorrect player name! Try again!\"}");
+                        resp.getWriter().write("{ \"status\": \"FAIL\",\"message\":\"Incorrect player name or player not exists! Try again!\"}");
                         return;
                     }
                 }
             }
 
             if(0!=scoreTwo){
-                String [] twoTeam = secondTeamPlayers.split(";");
-
+                String [] twoTeam = secondTeamPlayers.split(";");//All players that score goals at second team
+                //Search if such player exist in system(db) and then (if exists) search this player in second team
                 for(String playerName:twoTeam){
                     Player player = playerService.getPlayerByName(playerName);
                     if(player!=null) {
-                        secondTeam.add(player);
+                        boolean isThisPlayerAtTeam = false;
+                        for(Player p:allSecondTeamPlayers){
+                            if(p.getId()==player.getId()){
+                                isThisPlayerAtTeam = true;
+                                break;
+                            }
+                        }
+                        if(isThisPlayerAtTeam) {
+                            secondTeamGoals.add(player);
+                        }else{
+                            resp.getWriter().write("{ \"status\": \"FAIL\",\"message\":\"" + playerName + " is not a member of first team!!\"}");
+                            return;
+                        }
                     }else{
-                        resp.getWriter().write("{ \"status\": \"FAIL\",\"message\":\"Incorrect player name! Try again!\"}");
+                        resp.getWriter().write("{ \"status\": \"FAIL\",\"message\":\"Incorrect player name or player not exists! Try again!\"}");
                         return;
                     }
                 }
@@ -91,11 +123,11 @@ public class AddWinnerServlet extends HttpServlet{
 
             GoalService goalService = new GoalService();
 
-            for(Player player:firstTeam){
+            for(Player player:firstTeamGoals){
                 goalService.addGoal(gameID,firstTeamID,player.getId());
             }
 
-            for(Player player:secondTeam){
+            for(Player player:secondTeamGoals){
                 goalService.addGoal(gameID,secondTeamID,player.getId());
             }
 
