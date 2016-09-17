7c5c6260a307ff4c81a5c76d900be991b32c648c
roman <romanmr01@gmail.com>
2016-09-17 20:42:19 +0300

    added main functionality

--- src/main/java/com/epam/javalab13/servlet/client/ClientMatchesDispatcher.java
+++ src/main/java/com/epam/javalab13/servlet/client/ClientMatchesDispatcher.java
@@ -4,11 +4,13 @@ import com.epam.javalab13.dao.game.GameDAO;
 import com.epam.javalab13.model.game.Game;
 import com.epam.javalab13.service.ClientBetsService;
 import com.epam.javalab13.service.game.GameService;
+import com.epam.javalab13.service.game.UserService;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -35,6 +37,31 @@ public class ClientMatchesDispatcher extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("do post start");
 
+        HttpSession session = req.getSession();
+        String userLogin = (String) session.getAttribute("login");
+        int userID = new UserService().getUserByLogin(userLogin).getId();
+
+        String jsonArray = req.getParameter("requestJsonArray");
+        String betsCount = req.getParameter("betsCount");
+        String amountSum = req.getParameter("amount");
+        String awardSum = req.getParameter("award");
+
+        int betsSize = Integer.parseInt(betsCount);
+        int amount = Integer.parseInt(amountSum);
+        double award = Double.parseDouble(awardSum);
+
+        resp.setCharacterEncoding("UTF-8");
+
+
+        ClientBetsService clientBetsService = new ClientBetsService();
+        if(clientBetsService.makeBet(jsonArray,betsSize,amount,award,userID)){
+            resp.getWriter().write("{ \"status\": \"OK\",\"message\":\"Bet is done successfully!\"}");
+        }else{
+            resp.getWriter().write("{ \"status\": \"FAIL\",\"message\":\"Error, please try again later!\"}");
+        }
+
+        System.out.println("do post end");
     }
 }
